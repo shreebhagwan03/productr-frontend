c4c9656c42299917bbcc32f82f4c21a5897fe329
shreebhagwan <rankawatshree@gmail.com>
2026-02-02 22:46:19 +0530

    add new changes related to env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api"
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "http://localhost:5000/api" || process.env.REACT_APP_API_URL
 
 });
 
