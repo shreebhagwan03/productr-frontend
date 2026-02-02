f6d276cc072d969950f5cadccbb3c8ea6b2721d4
shreebhagwan <rankawatshree@gmail.com>
2026-02-02 19:29:00 +0530

    production build + api fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_API_URL
+
 });
 
 api.interceptors.request.use((config) => {
